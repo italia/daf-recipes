8dba71845afbf43f14c000d3bf8fc40f31834fa3
Alessandro Ercolani <alessandro@teamdigitale.governo.it>
2017-09-28 17:04:30 +0200

    freeipa from dockerhub

--- freeipa/startfreeipa.sh
+++ freeipa/startfreeipa.sh
@@ -1,3 +1,3 @@
-docker run -it -p 389:389 -p 443:443 -p 636:636 --name freeipaldap --cap-add SYS_ADMIN --security-opt seccomp:unconfined -v /sys/fs/cgroup:/sys/fs/cgroup:ro --tmpfs /run --tmpfs /tmp -v /var/lib/ipa-data:/data:Z -h ipa.example.test freeipa-server --ds-password=dspassword --admin-password=adminpassword
+docker run -it -p 389:389 -p 443:443 -p 636:636 --name freeipaldap --cap-add SYS_ADMIN --security-opt seccomp:unconfined -v /sys/fs/cgroup:/sys/fs/cgroup:ro --tmpfs /run --tmpfs /tmp -v /var/lib/ipa-data:/data:Z -h ipa.example.test italia/freeipa-server --ds-password=dspassword --admin-password=adminpassword
 
 #docker run -it -p 389:389 -p 443:443 -p 636:636 --name freeipaldap --cap-add SYS_ADMIN --security-opt seccomp:unconfined -v /sys/fs/cgroup:/sys/fs/cgroup:ro --tmpfs /run --tmpfs /tmp -v /opt/ipa-data:/data:Z -h ipa.example.test freeipa-server --ds-password=dspassword --admin-password=adminpassword
