ba78d272b0affaa20e2a50210a28b31312d1134f
fabiana <fabiana.lan@hotmail.it>
2017-10-03 15:38:17 +0200

    fabiana commit

--- jupyterhub/ldap_fake/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p db
-
-docker run -p 389:389 -v "$PWD/db:/var/lib/ldap"\
- -e LDAP_DOMAIN=mycorp.com\
- -e LDAP_ROOTPASS=1234abc\
- --name=fake_ldap
- --network=bridge -d nickstenning/slapd
