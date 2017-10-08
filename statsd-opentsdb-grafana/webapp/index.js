035b477e21060df47c97ea1cfa3e785b86058ebb
Nicola Salsotto <nicola@cryptolab.net>
2017-10-08 10:34:05 +0200

    Token based auth working based on URL

--- statsd-opentsdb-grafana/webapp/index.js
+++ statsd-opentsdb-grafana/webapp/index.js
@@ -6,15 +6,28 @@ var cors = require('cors')
 var app = express()
 const { spawn } = require('child_process');
 
-var APIkey;
+var APIkey = "-";
+
+//Generate the image every 5 seconds
+function generateImage(arg) {
+  exec('curl -H "Authorization: Bearer ' + APIkey + '" "http://localhost:3000/render/dashboard-solo/db/vecchio?panelId=1&orgId=1&from=1507408937119&to=1507409826503&width=1000&height=500&tz=UTC%2B02%3A00" > snapshot.png', puts);
+}
+setInterval(generateImage, 5000);
 
 //Verifies that the user is allowed to see graphs
 function verify(key) {
   var request = require('sync-request');
+
+  //'eyJrIjoiTG5Jck5JVW5BTVRwMDhJdW85MzZCQmw1NnR1aU02ZDciLCJuIjoibXlrZXkiLCJpZCI6MX0='
+
+  var string = 'Bearer ' + APIkey;
+
+  var headers = {
+    'Authorization': string
+  };
+
   var res = request('GET', 'http://localhost:3000', {
-  'headers': {
-    'Authorization': 'Bearer eyJrIjoiTG5Jck5JVW5BTVRwMDhJdW85MzZCQmw1NnR1aU02ZDciLCJuIjoibXlrZXkiLCJpZCI6MX0='
-  }
+  'headers': headers
 });
 
   if (res.statusCode == 200) {
@@ -50,6 +63,7 @@ function sendGraph(req, res, next) {
     res.send("Unauthorized");
   }
 
+  //generateImage();
 }
 
 app.get('/snapshot.png', sendSnapshot);
@@ -72,11 +86,3 @@ app.listen(3001, function () {
 var sys = require('sys')
 var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { console.log(stdout) }
-
-//Generate the image every 5 seconds
-function myFunc(arg) {
-  exec('curl -H "Authorization: Bearer eyJrIjoiTG5Jck5JVW5BTVRwMDhJdW85MzZCQmw1NnR1aU02ZDciLCJuIjoibXlrZXkiLCJpZCI6MX0=" "http://localhost:3000/render/dashboard-solo/db/vecchio?panelId=1&orgId=1&from=1507408937119&to=1507409826503&width=1000&height=500&tz=UTC%2B02%3A00" > snapshot.png', puts);
-}
-setInterval(myFunc, 5000);
-
-//res.send("<html> <head> </head> <body> <script> var xhr = new XMLHttpRequest(); xhr.responseType = 'blob'; xhr.onreadystatechange = function () { if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) { var img = document.createElement('img'); img.src = URL.createObjectURL(xhr.response); document.body.appendChild(img); } }; xhr.open('GET', 'http://localhost:3000/render/dashboard-solo/db/vecchio', true); xhr.setRequestHeader('Authorization', 'Bearer
